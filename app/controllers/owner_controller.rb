236ac93fda2ce7a13b2cc3b421a9f0bd53484d5b
stuckiest <stuckiest@gmail.com>
2015-09-04 15:24:40 -0600

    added static pages controller

--- app/controllers/owner_controller.rb
+++ app/controllers/owner_controller.rb
@@ -10,4 +10,23 @@ class OwnerController < ApplicationController
   def new
     @owner = Owner.new
   end
+
+ def create
+    @owner = Owner.new(owner_params)
+  
+    if @owner.save
+      redirect_to owners_path
+    else
+      render :new
+    end
+  end
+
+
+
+    private
+
+  def owner_params
+    params.require(:owner).permit(:name, :age, :profession)
+  end
+
 end
--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -10,4 +10,23 @@ class ProjectController < ApplicationController
   def new
     @project = Project.new
   end
+
+   def create
+    @project = Project.new(project_params)
+  
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+  
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :grade, :prize, :notes, :owner_id)
+  end
+
+
 end
